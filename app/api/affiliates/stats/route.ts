d30159d0b397e8eba4613d99776a9df30f28866d
GrewBie <dev@grewbie.com>
2025-12-17 22:35:06 +0530

    s3

--- app/api/affiliates/stats/route.ts
+++ app/api/affiliates/stats/route.ts
@@ -1,5 +1,6 @@
 
 import { NextRequest, NextResponse } from "next/server";
+export const dynamic = 'force-dynamic';
 import { storage } from "@/lib/storage";
 import { cookies } from "next/headers";
 import { verifyToken } from "@/lib/auth";
